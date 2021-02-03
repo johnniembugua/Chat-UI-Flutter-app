245a255c0729d052cc87a74292e859ef19bea840
John Mbugua <48179354+johnniembugua@users.noreply.github.com>
2021-02-03 16:44:38 +0300

    Updated the chatscreen.dart file
    I introduced the input text field whereby a user could type in text easily. I also enabled the both the senders messages and recipient messages to appear on the chat screen widget.

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -13,7 +13,7 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   _buildMessage(Message message, bool isMe) {
-    return Container(
+    final Container msg = Container(
       margin: isMe
           ? EdgeInsets.only(
               top: 8.0,
@@ -23,9 +23,9 @@ class _ChatScreenState extends State<ChatScreen> {
           : EdgeInsets.only(
               top: 8.0,
               bottom: 8.0,
-              right: 80.0,
             ),
       padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+      width: MediaQuery.of(context).size.width * 0.75,
       decoration: BoxDecoration(
         color: isMe ? Theme.of(context).accentColor : Color(0xFFFFEFEE),
         borderRadius: isMe
@@ -42,7 +42,77 @@ class _ChatScreenState extends State<ChatScreen> {
                 bottomLeft: Radius.circular(15.0),
               ),
       ),
-      child: Text(message.text),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            message.time,
+            style: TextStyle(
+                color: Colors.blueGrey,
+                fontSize: 16.0,
+                fontWeight: FontWeight.w600),
+          ),
+          SizedBox(height: 5.0),
+          Text(
+            message.text,
+            style: TextStyle(
+                color: Colors.blueGrey,
+                fontSize: 16.0,
+                fontWeight: FontWeight.w600),
+          ),
+        ],
+      ),
+    );
+    if (isMe) {
+      return msg;
+    }
+    return Row(
+      children: <Widget>[
+        msg,
+        IconButton(
+          icon: message.isLiked
+              ? Icon(Icons.favorite)
+              : Icon(Icons.favorite_border),
+          iconSize: 30.0,
+          color: message.isLiked
+              ? Theme.of(context).primaryColor
+              : Colors.blueGrey,
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+
+  _buildMessageComposer() {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 8.0,
+      ),
+      height: 70.0,
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.photo),
+            iconSize: 25.0,
+            color: Theme.of(context).primaryColor,
+            onPressed: () {},
+          ),
+          Expanded(
+              child: TextField(
+            textCapitalization: TextCapitalization.sentences,
+            onChanged: (value) {},
+            decoration: InputDecoration.collapsed(
+              hintText: 'Send a message ...',
+            ),
+          )),
+          IconButton(
+              icon: Icon(Icons.send),
+              iconSize: 25.0,
+              color: Theme.of(context).primaryColor,
+              onPressed: null)
+        ],
+      ),
     );
   }
 
@@ -68,35 +138,40 @@ class _ChatScreenState extends State<ChatScreen> {
           ),
         ],
       ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30.0),
-                  topRight: Radius.circular(30.0),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                  ),
                 ),
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30.0),
-                  topRight: Radius.circular(30.0),
-                ),
-                child: ListView.builder(
-                  padding: EdgeInsets.only(top: 15.0),
-                  itemCount: chats.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    final Message message = chats[index];
-                    final bool isMe = message.sender.id == currentUser.id;
-                    return _buildMessage(message, isMe);
-                  },
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                  ),
+                  child: ListView.builder(
+                    reverse: true,
+                    padding: EdgeInsets.only(top: 15.0),
+                    itemCount: messages.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final Message message = messages[index];
+                      final bool isMe = message.sender.id == currentUser.id;
+                      return _buildMessage(message, isMe);
+                    },
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+            _buildMessageComposer(),
+          ],
+        ),
       ),
     );
   }
